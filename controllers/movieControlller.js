dba72c293a6858a8b20381929a969343e27329d6
zahraa237 <fzahraa237@gmail.com>
2025-08-17 19:40:56 +0300

    finished the search functionality

--- controllers/movieControlller.js
+++ controllers/movieControlller.js
@@ -5,8 +5,9 @@ const apiKey = process.env.api_key;
 //searching
 const search = async (req, res) => {
   try {
+    const input = req.query.query;
     const result = await axios.get(
-      `${process.env.tmdbURL}search/movie?query=call&include_adult=false&language=en-US&page=1'&api_key=${apiKey}`
+      `${process.env.tmdbURL}search/movie?query=${input}&include_adult=false&language=en-US&page=1'&api_key=${apiKey}`
     );
     res.status(201).json(result.data);
   } catch (error) {
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const movieController = require("../controllers/movieControlller");
 
-router.get("/search/:query", movieController.search);
+router.get("/search", movieController.search);
 
 module.exports = router;
