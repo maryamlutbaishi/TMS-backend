95d644bdc53f0a7eab4d6e8a6baa17f6686d013d
maryam <maryamlutbaishi213@hotmail.com>
2025-08-18 09:59:17 +0300

    creat review router

---
+++ reviewRoutes.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const router = express.Router()
+const reviewController=require('../reviews')
+
+router.post('/', reviewController.creatreview)
+router.get('/',reviewController.reviewIndex)
+router.get('/:id',reviewController.showreview)
+router.delete('/:id',reviewController.deletreview)
+router.put('/:id',reviewController.updatereview)
+
+module.exports=router
