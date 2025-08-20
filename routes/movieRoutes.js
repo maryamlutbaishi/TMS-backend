05326c2d78b9c9c0b0d54f0682576f95b03dba0a
zahraa <fzahraa237@gmail.com>
2025-08-20 13:15:23 +0300

    add a movie tp the database

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const movieController = require("../controllers/movieControlller");
 
 router.get("/search", movieController.search);
-router.get("/:id",movieController.details)
-router.get("/:id/credits", movieController.movieCast)
+router.get("/:id", movieController.details);
+router.get("/:id/credits", movieController.movieCast);
+router.post("/add", movieController.create);
 module.exports = router;
